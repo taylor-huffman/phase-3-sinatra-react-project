5dcd181fb001019b94f2577ea50417c1fabfc3f2
Taylor Huffman <taylor@crucialartists.com>
2022-06-20 10:07:36 -0500

    Add initial get requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,24 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/teachers" do
+    teachers = Teacher.all
+    teachers.to_json
+  end
+
+  get "/students" do
+    students = Student.all
+    students.to_json
+  end
+
+  get "/subjects" do
+    subjects = Subject.all
+    subjects.to_json
+  end
+
+  get "/appointments" do
+    appointments = Appointment.all
+    appointments.to_json
   end
 
 end
