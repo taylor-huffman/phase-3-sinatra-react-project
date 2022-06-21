b64830676905b1f53084b3eb18a6469e84896e01
Taylor Huffman <taylor@crucialartists.com>
2022-06-21 12:55:14 -0500

    Add first instance method

--- app/models/Teacher.rb
+++ app/models/Teacher.rb
@@ -2,4 +2,20 @@ class Teacher < ActiveRecord::Base
     has_many :appointments
     has_many :students, through: :appointments
     has_many :subjects, through: :appointments
+
+    # def self.all_appointments
+    #     all_appointments = []
+    #     all.map do |t|
+    #         if t.appointments != []
+    #             all_appointments << t.appointments
+    #         else
+    #             false
+    #         end
+    #     end
+    #     all_appointments
+    # end
+
+    def all_appointments
+        self.appointments
+    end
 end
