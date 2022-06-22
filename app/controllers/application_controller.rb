7f88a6e6e2efbfca4c8daa211b6fed2505c7d228
Taylor Huffman <taylor@crucialartists.com>
2022-06-22 12:58:51 -0500

    Fix routes in contoller file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,20 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get '/teachers' do
+  get '/teachers/all' do
     teachers = Teacher.all
     teachers.to_json
   end
 
+  get '/teachers/find' do
+    name = params[:name]
+    teacher = Teacher.find_by(name: name)
+    teacher.to_json
+  end
+
   get '/teachers/:id' do
     teacher = Teacher.find(params[:id])
-    teacher.to_json
+    teacher.to_json(include: { appointments: { include: [:student, :subject] } })
   end
 
   post '/teachers' do
@@ -25,9 +31,20 @@ class ApplicationController < Sinatra::Base
     teacher.to_json
   end
 
-  get '/students' do
+  get '/students/all' do
     students = Student.all
     students.to_json
+  end
+
+  get '/students/find' do
+    name = params[:name]
+    student = Student.find_by(name: name)
+    student.to_json
+  end
+
+  get '/students/:id' do
+    student = Student.find(params[:id])
+    student.to_json(include: { appointments: { include: [:teacher, :subject] } })
   end
 
   post '/students' do
@@ -48,36 +65,36 @@ class ApplicationController < Sinatra::Base
     subjects.to_json
   end
 
-  get '/appointments' do
-    appointments = Appointment.all
-    appointments.to_json
-  end
+  # get '/appointments' do
+  #   appointments = Appointment.all
+  #   appointments.to_json
+  # end
 
-  post '/appointments' do
-    appointment = Appointment.create(
-      date: params[:date],
-      student_id: params[:student_id],
-      teacher_id: params[:teacher_id],
-      subject_id: params[:subject_id]
-    )
-    appointment.to_json
-  end
+  # post '/appointments' do
+  #   appointment = Appointment.create(
+  #     date: params[:date],
+  #     student_id: params[:student_id],
+  #     teacher_id: params[:teacher_id],
+  #     subject_id: params[:subject_id]
+  #   )
+  #   appointment.to_json
+  # end
 
-  patch '/appointments/:id' do
-    appointment = Appointment.find(params[:id])
-    appointment.update(
-      date: params[:date],
-      student_id: params[:student_id],
-      teacher_id: params[:teacher_id],
-      subject_id: params[:subject_id]
-    )
-    appointment.to_json
-  end
+  # patch '/appointments/:id' do
+  #   appointment = Appointment.find(params[:id])
+  #   appointment.update(
+  #     date: params[:date],
+  #     student_id: params[:student_id],
+  #     teacher_id: params[:teacher_id],
+  #     subject_id: params[:subject_id]
+  #   )
+  #   appointment.to_json
+  # end
 
-  delete '/appointments/:id' do
-    appointment = Appointment.find(params[:id])
-    appointment.destroy
-    appointment.to_json
-  end
+  # delete '/appointments/:id' do
+  #   appointment = Appointment.find(params[:id])
+  #   appointment.destroy
+  #   appointment.to_json
+  # end
 
 end
