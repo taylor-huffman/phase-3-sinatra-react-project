ffd2552b9684f2be811d8e44f8f3efa3edbc3432
Taylor Huffman <taylor@crucialartists.com>
2022-07-06 16:15:17 -0500

    Remove unnecessary code from models

--- app/models/Teacher.rb
+++ app/models/Teacher.rb
@@ -3,20 +3,4 @@ class Teacher < ActiveRecord::Base
     has_many :students, through: :appointments
     has_many :subjects, through: :appointments
     belongs_to :user_role
-
-    # def self.all_appointments
-    #     all_appointments = []
-    #     all.map do |t|
-    #         if t.appointments != []
-    #             all_appointments << t.appointments
-    #         else
-    #             false
-    #         end
-    #     end
-    #     all_appointments
-    # end
-
-    def all_appointments
-        self.appointments
-    end
 end
